247d0145c79c4ef706873fdb67cb461c47c47370
Alexander Kekh <kurt_alex@mail.ru>
2020-08-14 18:01:15 +0300

    Created abstract model ItemBase for uses different types on a model

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 """
 owner - пользователь, который создал курс
@@ -9,6 +11,7 @@ slug - слаг курса, будет использоваться для фо�
 overview - текстовое поле для создания краткого описания курса
 created - дата и время создания курса ПРОСТАВЛЯЮТСЯ АВТОМАТИЧЕСКИ
 """
+
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -45,3 +48,39 @@ class Module(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'objects_id')
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name='%(class)s_related', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+
+class Video(ItemBase):
+    url = models.URLField()
